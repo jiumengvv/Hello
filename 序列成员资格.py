694cff843c036931bc7d18d5c829cf3c5db4bfe3
jiumeng <1040412869@qq.com>
2016-07-20 14:08:16 +0800

    play

---
+++ 序列成员资格.py
@@ -0,0 +1,9 @@
+database=[
+  ['jiumeng', '951003'],
+  ['TINGHAO', '951124']
+]
+username=raw_input('Username:')
+pin=raw_input('PIN code:')
+
+if[username,pin]in database:print'Acess granted'
+if[username,pin] 
