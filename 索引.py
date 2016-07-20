b4918aebc6150e242f7fc02c1c6ab09ccefd3dc3
jiumeng <1040412869@qq.com>
2016-07-20 13:41:17 +0800

    索引,py

---
+++ 索引.py
@@ -0,0 +1,28 @@
+months=[
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'November',
+    'December',
+]
+
+endings=['st','nd','rd']+17*['th']\
+ +['st','nd','rd']+7*['th']\
+ +['st']
+year=raw_input('year:')
+month=raw_input('month(1-12):')
+day=raw_input('Day(1-31):')
+month_number=int(month)
+day_number=int(day)
+
+month_name=months[month_number-1]
+ordinal=day+endings[day_number-1]
+
+print month_name+' '+ordinal+', '+year
+
